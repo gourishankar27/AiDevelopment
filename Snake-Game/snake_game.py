3a2bc94a93ab79903a75157ca5022aa8a9432dac
gourishankar27 <gourishankar2710@gmail.com>
2024-01-09 23:32:28 +0530

    Added class FRUIT to rabdomly draw the fruits

--- Snake-Game/snake_game.py
+++ Snake-Game/snake_game.py
@@ -1,30 +1,37 @@
 import pygame
+import random
 import sys # gives access to lots of systems functionality. 
+from pygame.math import Vector2
+
+
+
+class FRUIT:
+    def __init__(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),
+                                 int(self.pos.y * cell_size),
+                                 cell_size,
+                                 cell_size)
+
+        pygame.draw.rect(screen,(126,166,144), fruit_rect)
+
 
 #it starts the pygame 
 pygame.init()
 
+cell_size = 40
+cell_number = 20
 # Display surface. # set_mode((width,height))
-screen = pygame.display.set_mode((600,700))
+screen = pygame.display.set_mode((cell_number*cell_size, cell_number*cell_size))
 
 clock = pygame.time.Clock()
 
-"""
-Display the surface 
-( This could be the display surface or a regular surface )
-"""
-
-test_surface = pygame.Surface((250,250))
-test_surface.fill((100,0,255))
-
-
-# test_rect = pygame.Rect(100,200,100,100)
-
-# test_rect = test_surface.get_rect( center = (400,250) )
-test_rect = test_surface.get_rect( topright = (400,250) )
-
-x_position = 0
-y_position = 0
+fruit = FRUIT()
 
 while True:
     # HEre draw all the elements for game. 
@@ -38,17 +45,7 @@ while True:
     # screen.fill(pygame.Color('yellow'))
     screen.fill((175,215,70))
 
-    # x_position += 1
-    # y_position += 2
-
-    test_rect.right += 1 
-
-    # pygame.draw.rect(screen,(255,0,50), test_rect)
-    # pygame.draw.ellipse(screen,(255,0,50), test_rect)
-
-    screen.blit(test_surface,test_rect)
-
-
+    fruit.draw_fruit()
 
     pygame.display.update()
 
