3f6e591bf4a8d877577438db392d3f9eba6d7ac2
gourishankar27 <gourishankar2710@gmail.com>
2024-01-09 23:56:37 +0530

    Added class SNAKE to draw a snake

--- Snake-Game/snake_game.py
+++ Snake-Game/snake_game.py
@@ -4,21 +4,44 @@ import sys # gives access to lots of systems functionality.
 from pygame.math import Vector2
 
 
+class SNAKE:
+    def __init__(self):
+        # initializing with starting position of snake
+        self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
+
+    def draw_snake(self):
+        for block in self.body:
+            # create a rectangle 
+            # draw the rectangle 
+
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+
+            snake_rect = pygame.Rect(x_pos, y_pos, 
+                                     cell_size, cell_size)
+        
+            pygame.draw.rect(screen, (100,20,255), snake_rect)
+
 
 class FRUIT:
     def __init__(self):
         self.x = random.randint(0, cell_number - 1)
         self.y = random.randint(0, cell_number - 1)
 
+        print(f"FRUIT position x:{self.x} , y:{self.y}")
+
         self.pos = Vector2(self.x, self.y)
 
     def draw_fruit(self):
-        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),
-                                 int(self.pos.y * cell_size),
-                                 cell_size,
-                                 cell_size)
+        # create a rectangle 
+        # draw the rectangle 
+        x_pos = int(self.pos.x * cell_size)
+        y_pos = int(self.pos.y * cell_size)
 
-        pygame.draw.rect(screen,(126,166,144), fruit_rect)
+        fruit_rect = pygame.Rect(x_pos, y_pos ,
+                                 cell_size, cell_size)
+
+        pygame.draw.rect(screen,(255,30,100), fruit_rect)
 
 
 #it starts the pygame 
@@ -33,6 +56,8 @@ clock = pygame.time.Clock()
 
 fruit = FRUIT()
 
+snake = SNAKE()
+
 while True:
     # HEre draw all the elements for game. 
 
@@ -46,6 +71,7 @@ while True:
     screen.fill((175,215,70))
 
     fruit.draw_fruit()
+    snake.draw_snake()
 
     pygame.display.update()
 
